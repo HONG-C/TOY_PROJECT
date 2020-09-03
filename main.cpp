9b40594480329978ddcd805f6865a005062431a5
jihunHong <jihun3900@naver.com>
2020-09-03 14:50:25 +0000

    2020.09.03 원하는 위치로 이동하는 함수 move 제작

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
+/*
+프로젝트명:산성비(acid rain)
+시작날짜:2020년 9월 3일
+*/
+
 #include <iostream>
 
-int main() {
+using namespace std;
+
+void move(int x,int y)
+{
+  for(int j=1;j<y;j++)
+  {
+  cout<<"1"<<endl;
+  }
+  for(int i=0;i<x;i++)
+  {
+  cout<<"2";
+  }
+}
+
+int main() 
+{ 
+  move(3,3);
   std::cout << "Hello World!\n";
 }
