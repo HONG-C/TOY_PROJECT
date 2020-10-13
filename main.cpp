b997b808236aa4f963d7a83382367c3423eb302d
jihunHong <jihun3900@naver.com>
2020-10-13 13:48:37 +0000

    10월 13일 추가 변경사항 -쓰레드 부분 삭제

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <cstdlib>
 #include <stdlib.h>
 #include <ctime>
-#include <thread>
+
 
 using namespace std;
 
@@ -16,15 +16,6 @@ void mysleep(int sec);
 void move(int x,int y);
 void rain(char *text[],int i);
 int check_typing(void);
-void test_thread(void)
-{
-  cout<<"this is thread";
-}
-void test_thread_2(void)
-{
-  cout<<"this is  also thread";
-}
-
 
 char *raining_word="hello world";//상수형태의 문자열이라 오류가 발생하는듯..수정필요
 
@@ -125,14 +116,9 @@ int check_typing(void)
 
 int main() 
 { 
-  /*
+  
   raining_word=word[0];
   int check_num=check_typing();
   rain(word,check_num);//키포인트:&word[0]=word,word[i]=*(word+i)
-  */
-  thread t1(test_thread);
-  thread t2(test_thread_2);
-  
-  t1.join();
-  t2.join();
+
 }
